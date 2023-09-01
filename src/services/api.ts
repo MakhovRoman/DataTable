cd7bcaa7bafbd90ac1a7798df8bb4a9d327d4ed8
Roman Makhov <nekromantos30888666@gmail.com>
2023-09-01 09:39:08 +0300

    feat: add try/catch

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,27 @@
 import axios from "axios";
 
+export class ApiError extends Error {
+  constructor(public message: string, public status: number) {
+    super(message);
+
+    this.name = "ApiError";
+    this.status = status;
+  }
+}
+
 export const API = {
   load: async (url: string) => {
-    const data = await axios.get(`${url}`);
+    try {
+      const data = await axios.get(`${url}`);
 
-    return data.data;
+      return data.data;
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        // ApiError, чтобы была возможность фильтровать ошибки, если нужно
+        return Promise.reject(new ApiError(error.response.data.reason, error.response.status));
+      }
+
+      return Promise.reject(error);
+    }
   },
 }
